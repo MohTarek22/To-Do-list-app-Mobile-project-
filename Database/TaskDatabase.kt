225f3a09106fa9f2b0115d039db9ad9880635759
Amr Alattar <130612395+Alattar22@users.noreply.github.com>
2025-12-15 08:50:36 +0200

    Add files via upload
    database is doen

---
+++ Database/TaskDatabase.kt
@@ -0,0 +1,27 @@
+package com.example.todolist.Database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.todolist.ViewModel.Task
+
+@Database(entities = [Task::class], version = 1, exportSchema = false)
+abstract class TaskDatabase : RoomDatabase() {
+
+    abstract fun taskDao(): TaskDao
+
+    companion object {
+        @Volatile
+        private var Instance: TaskDatabase? = null
+
+        fun getDatabase(context: Context): TaskDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, TaskDatabase::class.java, "task_database")
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+}
